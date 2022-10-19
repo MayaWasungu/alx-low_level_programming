953576367d71da924f8801d37753224662a5313e
MayaWasungu <myawasungu@gmail.com>
2022-10-19 15:18:42 -0700

    task 4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -7,7 +7,7 @@
  *  Returns: 1 if char is a letter lower or upper, 0 otherwise
  */
 
-int _isalpha(int c);
+int _isalpha(int c)
 {
 	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'z'))
 		return (1);
