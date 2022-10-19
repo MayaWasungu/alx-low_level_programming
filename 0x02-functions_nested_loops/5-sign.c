bbc6e8859a361ecd7901af96cedbda0b40ee0995
MayaWasungu <myawasungu@gmail.com>
2022-10-19 16:10:38 -0700

    task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,7 +3,7 @@
 /**
  * print_sign - prints the sign of a num
  * @n: the num to be cecked for
- * Returns 1 and prints + if n is greater than zero
+ * Return: 1 and prints + if n is greater than zero
  * 0 and prints 0 if n is zero
  * -1 and prints - if n is less than zero
  */
@@ -12,17 +12,17 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		 _putchar('+');
-		 return(1);
+		_putchar('+');
+		return (1);
 	}
 	else if (n == 0)
 	{
 		_putchar('0');
- 		return(0);
+		return (0);
 	}
 	else
 	{
-		 _putchar('-');
-		 return(-1);
+		_putchar('-');
+		return (-1);
 	}
 }
