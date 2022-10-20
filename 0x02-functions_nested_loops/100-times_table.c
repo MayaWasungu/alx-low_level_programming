bef734aed514bc38599d7b5c2702692a54f84f03
MayaWasungu <myawasungu@gmail.com>
2022-10-20 11:31:00 -0700

    task 12 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -14,7 +14,8 @@ void print_times_table(int n)
 		return; /**if the condtn above is satisfied, do NOTHING **/
 	while (x <= n)
 	{
-		for (y = 0; y <= n; y++);
+		for (y = 0; y <= n; y++)
+
 		{
 			z = x * y;
 			if (z > 99)
