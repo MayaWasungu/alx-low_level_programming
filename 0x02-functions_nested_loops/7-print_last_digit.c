10050d7fde4c4333f708000bba05a800d3d6567f
MayaWasungu <myawasungu@gmail.com>
2022-10-20 10:08:24 -0700

    task 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,7 +8,7 @@
 
 int print_last_digit(int n)
 {
-	int last digit;
+	int last_digit;
 
 	if (n < 0)
 	{
@@ -21,7 +21,6 @@ int print_last_digit(int n)
 	{
 		last_digit = (n % 10);
 		_putchar (last_digit + '0');
-		return (last_digit);
+		return (0);
 	}
-
 }
