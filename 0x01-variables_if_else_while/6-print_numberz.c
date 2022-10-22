67fbf1c3ea6e03263459204f1913712f007fa197
MayaWasungu <myawasungu@gmail.com>
2022-10-22 11:52:40 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success/corect)
+ */
+
+int main(void)
+
+{
+	int c;
+
+	for (c = 48 ; c <= 57; c++)
+	{
+		putchar (c);
+	}
+
+	putchar ('\n');
+
+	return (0);
+
+}
