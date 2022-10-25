b4f729e66ca593c5f7dbd20504d58c836bcb725b
MayaWasungu <myawasungu@gmail.com>
2022-10-25 11:22:41 -0700

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _isupper - evalue if letter is uppercase .
+ * @c: char to check
+ * 
+ * Return: Always 0.
+ */
+
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+
+	else
+	{
+		return (0);
+	}
+
+}
