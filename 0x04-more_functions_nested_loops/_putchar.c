891b11f44487c7d7ad21a7406e9ed1a5046891c1
MayaWasungu <myawasungu@gmail.com>
2022-10-27 10:30:45 -0700

    function that prints the numbers, from 0 to 9, followed by a new line.

---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+                                                                                                     
+                                                                                                                             
+/**                                                                                                                          
+ * print_numbers - print numbers from 0-9.                                                                                   
+ */                                                                                                                          
+                                                                                                                             
+void print_numbers(void)                                                                                                     
+{                                                                                                                            
+	int n;                                                                                                               
+		                                                                                                                             
+	for (n = 0, n<= 9, n++)                                                                                              
+	{                                                                                                                    
+		putchar((n % 10) + '0');                                                                                    
+	}                                                                                                                    
+	_putchar('\n');                                                                                                      
+}
