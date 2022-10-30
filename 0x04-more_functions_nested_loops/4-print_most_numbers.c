7e4a4c970502970ef08507f6e7b3186201844c57
MayaWasungu <myawasungu@gmail.com>
2022-10-30 08:29:57 -0700

    task 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_most_numbers - print numbers
+ *
+ * Return: Always 0.
+ */
+
+void print_most_numbers(void)
+{
+
+	int i;
+
+	i = 0;
+	while (i < 10)
+	{
+		if (i != 2 && i != 4)
+		{
+			putchar (i + '0');
+		}
+		i++;
+	}
+
+	putchar ('\n');
+}
